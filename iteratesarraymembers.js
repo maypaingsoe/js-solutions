8c5a2530fe32d177a26cf6cfb936dfedc480250f
maypaing (maddy) <64676121+maypaingsoe@users.noreply.github.com>
2021-01-15 16:41:13 +0630

    initial commit

---
+++ iteratesarraymembers.js
@@ -0,0 +1,7 @@
+/*write a for loop that iterates on the myArray variable and prints out all of its members.
+ */
+
+var myArray = ['What is the meaning of life?', 'The meaning of life is', 42];
+for (var i in myArray) {
+  console.log(myArray[i]);
+}
