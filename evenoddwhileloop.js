f11036caf16c96e9d4b91a036b2b801021acc5b6
maypaing (maddy) <64676121+maypaingsoe@users.noreply.github.com>
2021-01-21 17:22:14 +0630

    initial commit

---
+++ evenoddwhileloop.js
@@ -0,0 +1,10 @@
+var num = [1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var i = 0;
+while (num[i]) {
+  if (num[i] % 2 == 0) {
+    console.log(num[i] + ' even.');
+  } else {
+    console.log(num[i] + ' odd.');
+  }
+i++;
+}
